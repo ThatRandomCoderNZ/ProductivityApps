a0b80ec6eafcb55b215d207832b0ad77b33dd287
ThatRandomCoderNZ <ethan.en.mitchell@gmail.com>
2021-03-10 20:41:39 +1300

    feat - Added functionality to create a group of tabs

--- ProductivityExtension/popup.js
+++ ProductivityExtension/popup.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const actionButton = document.getElementById("call-to-action");
     const text = document.getElementById("test-text");
     actionButton.addEventListener('click', () => {
-        text.textContent = "Test This!";
-        console.log("Hello World!");
+
+        let tabsToOpen = ["youtube.com", "facebook.com", "twitter.com"];
+
+        for (let i in tabsToOpen){
+            let newURL = "http://" + tabsToOpen[i] + "/";
+            chrome.tabs.create({ url: newURL });
+        }
+
+
+        text.textContent = "Opened Tabs!";
     })
 })
