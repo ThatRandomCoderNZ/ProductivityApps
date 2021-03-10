4b5c91472c71b3f036f15651c2deb304f9e76d49
ThatRandomCoderNZ <ethan.en.mitchell@gmail.com>
2021-03-10 22:29:46 +1300

    feat - Added group functionality where tabs created at the same time can be part of the same group.

--- ProductivityExtension/popup.js
+++ ProductivityExtension/popup.js
@@ -1,16 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+
+
+
     const actionButton = document.getElementById("call-to-action");
     const text = document.getElementById("test-text");
-    actionButton.addEventListener('click', () => {
+    actionButton.addEventListener('click', async () => {
 
         let tabsToOpen = ["youtube.com", "facebook.com", "twitter.com"];
+        
+        chrome.tabs.create({ url: "http://" + tabsToOpen[0] + "/", active: false }, (firstTab) => {
+            chrome.tabs.group({ tabIds: firstTab.id }, (groupId) => {
+                for (let i = 1; i < tabsToOpen.length; i++){
+                    let newURL = "http://" + tabsToOpen[i] + "/";
+                    chrome.tabs.create({ url: newURL, active: false }, (newTab) => {
+                        console.log(groupId);
+                        chrome.tabs.group({ tabIds: newTab.id, groupId: groupId })
+                        
+                    });
+                }
+            });
+            
+        });
 
-        for (let i in tabsToOpen){
-            let newURL = "http://" + tabsToOpen[i] + "/";
-            chrome.tabs.create({ url: newURL });
-        }
+        console.log(createdTabIds.pop());
+        console.log([1 , 2, 3]);
 
+        
+        
 
         text.textContent = "Opened Tabs!";
     })
 })
+
+
+
+async function createTabs(tabsToOpen){
+    let createdTabIds = []
+    for (let i in tabsToOpen){
+        let newURL = "http://" + tabsToOpen[i] + "/";
+        chrome.tabs.create({ url: newURL, active: false }, (newTab) => {
+            createdTabIds.push(Number(newTab.id));
+            if (i == tabsToOpen.length - 1){
+                return createdTabIds;
+            }
+        });
+    }
+
+}
