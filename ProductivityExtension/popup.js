ab80e46ad2f9c4d89fe83545da12948b9699f783
ThatRandomCoderNZ <ethan.en.mitchell@gmail.com>
2021-09-09 12:03:58 +1200

    feat - minor revisions

--- ProductivityExtension/popup.js
+++ ProductivityExtension/popup.js
@@ -23,6 +23,28 @@ function toggleAccordian(container){
 
 }
 
+function deleteGroup(groupId, groupElement){
+    chrome.storage.sync.remove(groupId);
+    groupElement.remove();
+}
+
+function createTabs(groupId, groupData){
+    tabsToOpen = groupData["tabs"];
+    chrome.tabs.create({ url: tabsToOpen[0] + "/", active: false }, (firstTab) => {
+        chrome.tabs.group({ tabIds: firstTab.id }, (groupId) => {
+            for (let i = 1; i < tabsToOpen.length; i++){
+                let newURL = tabsToOpen[i] + "/";
+                chrome.tabs.create({ url: newURL, active: false }, (newTab) => {
+                    console.log(groupId);
+                    chrome.tabs.group({ tabIds: newTab.id, groupId: groupId }, (groupId) => {
+                        chrome.tabGroups.update(groupId, { title: groupData.title, color: groupData.color });
+                    });
+                });
+            };
+        });
+    });
+}
+
 function main(){
     document.querySelector("#reset-button").addEventListener('click', () => {
         console.log("Clicked");
@@ -33,34 +55,53 @@ function main(){
 
     chrome.storage.sync.get(null, (result) => {
         for(var obj in result){
-            if(obj != "allTabs"){
+            console.log(obj);
+            if(obj !== "allTabs"){
+                console.log("Putting in the work");
                 let group = document.createElement("div");
                 group.class = "group-div";
                 group.id = "group-" + obj;
                 
                 innerElements = `
-                <p>${result[obj].title}</p>
-                <button class="show">Show/Hide</button>
-                <button class="cancel"> Delete </button>
-                <div class="tab-container closed">
-                <ul>`;
-                
-                tabs = result[obj].tabs;
-                for(tab in tabs){
-                    innerElements +=`<li>
-                                        <div class="tab-logo-container"></div>
-                                        <p>${cleanURL(tabs[tab])}</p>
-                                     </li>`;
-                }
+                <details class="group-details">
+                    <summary>
+                    <div class="group-header">
+                    <p>${result[obj].title}</p>
+                    <button class="invis-button open-group"><img class="button-image"src="https://img.icons8.com/ios/50/000000/open-in-popup.png"/></button>
+                    <button class="invis-button delete"><img class="button-image" src="delete.png"></button>
+                    </div>
+                    </summary>
+                    <ul>`;
+                    
+                    tabs = result[obj].tabs;
+                    for(tab in tabs){
+                        innerElements +=
+                        `<li>
+                            <div class="tab-logo-container"></div>
+                            <p>${cleanURL(tabs[tab])}</p>
+                        </li>`;
+                    }
+    
+                    innerElements += "</ul></details>"
 
-                innerElements += "</ul></div>"
                 group.innerHTML = innerElements;
 
                 groupDiv.appendChild(group);
 
-                group.querySelector(".show").addEventListener('click', (e) => {
-                    toggleAccordian(group);
+                let data = result[obj];
+                const groupId = obj;
+                const groupElement = group;
+
+                group.querySelector(".open-group").addEventListener('click', (e) => {
+                    
+                    createTabs(groupId, data);
                 });
+
+                group.querySelector(".delete").addEventListener('click', (e) => {
+                    deleteGroup(groupId, groupElement);
+                });
+
+
             }
         }
     })
